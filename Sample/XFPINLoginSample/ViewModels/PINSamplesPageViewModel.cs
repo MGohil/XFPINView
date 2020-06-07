ad24ac7917bd383dc952f521f93f60f47415e168
MGohil <milan_213@yahoo.co.in>
2020-06-07 15:15:53 +0530

    Samples - AutoDissmissKeyboard and Input PIN Programatically samples added

--- Sample/XFPINLoginSample/ViewModels/PINSamplesPageViewModel.cs
+++ Sample/XFPINLoginSample/ViewModels/PINSamplesPageViewModel.cs
@@ -1,13 +1,17 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 
 namespace XFPINLoginSample.ViewModels
 {
     public class PINSamplesPageViewModel : BaseViewModel
     {
+        Random random = new Random();
+
         public PINSamplesPageViewModel()
         {
             Title = "PIN Samples";
             PINEntryCompletedCommand = new Command<string>(PINEntryCompletedCommandExecute);
+            InputPINProgramaticallyCommand = new Command(InputPINProgramaticallyCommandExecute);
         }
 
         string pin;
@@ -22,5 +26,41 @@ namespace XFPINLoginSample.ViewModels
         {
             await Application.Current.MainPage.DisplayAlert("Message", "PINEntryCompletedCommand Invoked", "OK");
         }
+
+
+
+        #region Input PIN Programatically
+        string randomPIN = "12345";
+
+        string pin1;
+        public string PIN1
+        {
+            get { return pin1; }
+            set { SetProperty(ref pin1, value); }
+        }
+
+        string pin2;
+        public string PIN2
+        {
+            get { return pin2; }
+            set { SetProperty(ref pin2, value); }
+        }
+
+        string inputPINButtonText = "Tap to Input : 12345";
+        public string InputPINButtonText
+        {
+            get { return inputPINButtonText; }
+            set { SetProperty(ref inputPINButtonText, value); }
+        }
+
+        public Command InputPINProgramaticallyCommand { get; set; }
+        private void InputPINProgramaticallyCommandExecute()
+        {
+            PIN1 = PIN2 = randomPIN;
+
+            randomPIN = random.Next(11111, 99999).ToString();
+            InputPINButtonText = $"Tap to Input : {randomPIN}";
+        }
+        #endregion
     }
 }
