cf8e3fe10ad213293772c7b0dcd21b2dd29c6b2d
MGohil <milan_213@yahoo.co.in>
2020-06-07 16:03:11 +0530

    Sample Added for PINInputType property example/demo

---
+++ Sample/XFPINLoginSample/Views/SampleViews/PINInputTypeSampleView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace XFPINLoginSample.Views.SampleViews
+{
+    public partial class PINInputTypeSampleView : ContentView
+    {
+        public PINInputTypeSampleView()
+        {
+            InitializeComponent();
+            pinView1.PINValue = "12345";
+            pinView2.PINValue = "ABCDE";
+        }
+    }
+}
