1f5f77fb4a5ac7fd5eda349c768fb44b9984cb8b
MGohil <milan_213@yahoo.co.in>
2020-06-13 20:44:47 +0530

    BoxBorderColor - Sample added.

---
+++ Sample/XFPINLoginSample/Views/SampleViews/BoxBorderColorSampleView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace XFPINLoginSample.Views.SampleViews
+{
+    public partial class BoxBorderColorSampleView : ContentView
+    {
+        public BoxBorderColorSampleView()
+        {
+            InitializeComponent();
+        }
+    }
+}
